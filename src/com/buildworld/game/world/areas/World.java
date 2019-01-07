17733acde1610da4437fbf59fdb7a5ed837854e1
ShawnClake <shawn.clake@gmail.com>
2019-01-06 18:10:20 -0600

    optimizations

--- src/com/buildworld/game/world/areas/World.java
+++ src/com/buildworld/game/world/areas/World.java
@@ -353,15 +353,25 @@ public class World implements IArea, IPersist {
         return count;
     }
 
+    /**
+     * Gets a cube region with a specified center point and extending in all directions by the radius amount
+     * @param x
+     * @param y
+     * @param z
+     * @param radius
+     * @return
+     * @throws Exception
+     */
     public Block[] getRegion(int x, int y, int z, int radius) throws Exception {
         int diameter = radius * 2 + 1;
         int start = radius * -1; // radius * -1
+        int yStart = (start < 0) ? 0 : start;
         List<Block> region = new ArrayList<>();
         for (int i = start; i <= radius; i++) {
             for (int j = start; j <= radius; j++) {
-                for (int k = 0; k < y; k++) {
+                for (int k = yStart; k < radius; k++) {
                     try {
-                        Block block = getBlock(x + i, k, z + j);
+                        Block block = getBlock(x + i, y + k, z + j);
                         if (block != null && getBlockNeighborCount(block) < 6) {
                             region.add(block);
                         }
@@ -373,6 +383,17 @@ public class World implements IArea, IPersist {
         return region.toArray(new Block[0]);
     }
 
+    /**
+     * Returns a cube region of blocks bounded by the two diagonal points
+     * @param x1
+     * @param y1
+     * @param z1
+     * @param x2
+     * @param y2
+     * @param z2
+     * @return
+     * @throws Exception
+     */
     public Block[] getRegion(int x1, int y1, int z1, int x2, int y2, int z2) throws Exception {
         List<Block> region = new ArrayList<>();
 
@@ -383,10 +404,12 @@ public class World implements IArea, IPersist {
         for (int i = xs; i <= Math.max(x1, x2); i++) {
             for (int j = zs; j <= Math.max(z1, z2); j++) {
                 for (int k = ys; k <= Math.max(y1, y2); k++) {
-                    Block block = getBlock(i, k, j);
-                    System.out.println(block.getName());
-                    if (block != null) {
-                        region.add(block);
+                    try {
+                        Block block = getBlock(i, k, j);
+                        if (block != null && getBlockNeighborCount(block) < 6) {
+                            region.add(block);
+                        }
+                    } catch(Exception e) {
                     }
                 }
             }
@@ -489,10 +512,154 @@ public class World implements IArea, IPersist {
         return coords.toArray(new Vector2f[0]);
     }
 
-    public Vector2f[] flipMovedRegionCoords(Vector2f[] coords, int x1, int z1, int radius, boolean xFlip, boolean zFlip) {
-        Vector2f[] flippedCoords = new Vector2f[coords.length];
+    /**
+     * Returns a list of coordinates consumed by the (X2, Y@, Z2) region which were not consumed by the (X1,Y1,Z1) region.
+     * All regions must be odd number sizes, this is enforced by passing in a radius.
+     * Algorithm is more efficient with smaller moves, and its worst cases are:
+     * [abs(x2-x1) >= (radius * 2 + 1), abs(y2-y1) >= (radius * 2 + 1), abs(z2-z1) >= (radius * 2 + 1)]
+     * Algorithm only iterates for each extra consumed coordinate.
+     *
+     * @param x1
+     * @param y1
+     * @param z1
+     * @param x2
+     * @param y2
+     * @param z2
+     * @param radius
+     * @return
+     */
+    public Vector3f[] getMovedRegionCoords(int x1, int y1, int z1, int x2, int y2, int z2, int radius) {
+        // If there is no movement, return an empty array
+        if (x1 == x2 && y1 == y2 && z1 == z2)
+            return new Vector3f[0];
 
+        List<Vector3f> coords = new ArrayList<>();
+
+        // diameter represents the side length of a square
         int diameter = radius * 2 + 1;
+
+        // whether we are flipping on the X and/or Z axis to get our L to the top right quadrant
+        boolean xFlip = false, yFlip=false, zFlip = false;
+
+        /*
+         * VECTOR LIST:
+         * P1: The original point
+         * P2: The point we are moving to
+         * P3: The point where P2 maps to the top right quadrant, if P2 is already in the top right, then P3==P2
+         * P4: A coordinate
+         * flip: The absolute values of the difference of (P2 - P1)
+         * reverseFlip: Maps P4 from the top right quadrant to its expected quadrant
+         */
+        Vector3f p1 = new Vector3f(x1, y1, z1);
+        Vector3f p2 = new Vector3f(x2, y2, z2);
+
+        Vector3f flip = new Vector3f(p2).sub(p1);
+
+        if (flip.x() < 0) {
+            xFlip = true;
+            flip.x = Math.abs(flip.x);
+        }
+
+        if (flip.y() < 0) {
+            yFlip = true;
+            flip.y = Math.abs(flip.y);
+        }
+
+        if (flip.z() < 0) {
+            zFlip = true;
+            flip.z = Math.abs(flip.z);
+        }
+
+        Vector3f p3 = new Vector3f(p1).add(flip);
+
+        // Gets the coordinates of the prism on the right of the cube
+        // Only runs if there is movement on the X axis
+        for (int i = 0; i < flip.x; i++) {
+            for (int j = 0; j < diameter; j++) {
+                for(int k = 0; k < diameter; k++)
+                {
+                    Vector3f reverseFlip = new Vector3f(0, 0, 0);
+
+                    Vector3f p4 = new Vector3f(p3).add(radius - flip.x() + 1, -radius, -radius).add(i, j, k);
+
+                    if (xFlip)
+                        reverseFlip.x = (p4.x() - p1.x()) * 2;
+                    if (yFlip)
+                        reverseFlip.y = (p4.y() - p1.y()) * 2;
+                    if (zFlip)
+                        reverseFlip.z = (p4.z() - p1.z()) * 2;
+
+                    p4.sub(reverseFlip);
+
+                    coords.add(p4);
+                }
+            }
+        }
+
+        // Gets the coordinates of the prism on the top of the cube
+        // Only runs if there is movement on the Y axis
+        for (int i = 0; i < (diameter - flip.x); i++) {
+            for (int j = 0; j < flip.y; j++) {
+                for(int k = 0; k < diameter; k++)
+                {
+                    Vector3f reverseFlip = new Vector3f(0, 0, 0);
+
+                    Vector3f p4 = new Vector3f(p3).add(-radius, radius - flip.y() + 1, -radius).add(i, j, k);
+
+                    if (xFlip)
+                        reverseFlip.x = (p4.x() - p1.x()) * 2;
+                    if (yFlip)
+                        reverseFlip.y = (p4.y() - p1.y()) * 2;
+                    if (zFlip)
+                        reverseFlip.z = (p4.z() - p1.z()) * 2;
+
+                    p4.sub(reverseFlip);
+
+                    coords.add(p4);
+                }
+
+            }
+        }
+
+        // Gets the coordinates of the prism on the back of the cube
+        // Only runs if there is movement on the Z axis
+        for (int i = 0; i < (diameter - flip.x); i++) {
+            for (int j = 0; j < (diameter - flip.y); j++) {
+                for(int k = 0; k < flip.z; k++)
+                {
+                    Vector3f reverseFlip = new Vector3f(0, 0, 0);
+
+                    Vector3f p4 = new Vector3f(p3).add(-radius, -radius, radius - flip.z() + 1).add(i, j, k);
+
+                    if (xFlip)
+                        reverseFlip.x = (p4.x() - p1.x()) * 2;
+                    if (yFlip)
+                        reverseFlip.y = (p4.y() - p1.y()) * 2;
+                    if (zFlip)
+                        reverseFlip.z = (p4.z() - p1.z()) * 2;
+
+                    p4.sub(reverseFlip);
+
+                    coords.add(p4);
+                }
+
+            }
+        }
+
+        return coords.toArray(new Vector3f[0]);
+    }
+
+    /**
+     * Flips a set of coordinates around the point x1,z1
+     * @param coords
+     * @param x1
+     * @param z1
+     * @param xFlip
+     * @param zFlip
+     * @return
+     */
+    public Vector2f[] flipMovedRegionCoords(Vector2f[] coords, int x1, int z1, boolean xFlip, boolean zFlip) {
+        Vector2f[] flippedCoords = new Vector2f[coords.length];
 
         Vector2f p1 = new Vector2f(x1, z1);
 
@@ -510,6 +677,44 @@ public class World implements IArea, IPersist {
         return flippedCoords;
     }
 
+    /**
+     * Flips a set of coordinates around the point x1,y1,z1
+     * @param coords
+     * @param x1
+     * @param y1
+     * @param z1
+     * @param xFlip
+     * @param yFlip
+     * @param zFlip
+     * @return
+     */
+    public Vector3f[] flipMovedRegionCoords(Vector3f[] coords, int x1, int y1, int z1, boolean xFlip, boolean yFlip, boolean zFlip) {
+        Vector3f[] flippedCoords = new Vector3f[coords.length];
+
+        Vector3f p1 = new Vector3f(x1, y1, z1);
+
+        for (int i = 0; i < coords.length; i++) {
+            Vector3f reverseFlip = new Vector3f(0, 0, 0);
+
+            if (xFlip)
+                reverseFlip.x = (coords[i].x() - p1.x()) * 2;
+            if(yFlip)
+                reverseFlip.y = (coords[i].y() - p1.y()) * 2;
+            if (zFlip)
+                reverseFlip.z = (coords[i].z() - p1.z()) * 2;
+
+            flippedCoords[i] = new Vector3f(coords[i]).sub(reverseFlip);
+        }
+
+        return flippedCoords;
+    }
+
+    /**
+     * Translates a set of coords by a specified offset
+     * @param coords
+     * @param offset
+     * @return
+     */
     public Vector2f[] translateRegionCoords(Vector2f[] coords, Vector2f offset) {
         Vector2f[] translatedCoords = new Vector2f[coords.length];
 
@@ -520,26 +725,61 @@ public class World implements IArea, IPersist {
         return translatedCoords;
     }
 
+    /**
+     * Translates a set of coords by a specified offset
+     * @param coords
+     * @param offset
+     * @return
+     */
+    public Vector3f[] translateRegionCoords(Vector3f[] coords, Vector3f offset) {
+        Vector3f[] translatedCoords = new Vector3f[coords.length];
+
+        for (int i = 0; i < coords.length; i++) {
+            translatedCoords[i] = new Vector3f(coords[i]).add(offset);
+        }
+
+        return translatedCoords;
+    }
+    
     public Block[] getMovedRegion(int x1, int y1, int z1, int x2, int y2, int z2, int radius) throws Exception {
-        return getMovedRegion(getMovedRegionCoords(x1, z1, x2, z2, radius), y1, y2);
+        return getMovedRegion(getMovedRegionCoords(x1, y1, z1, x2, y2, z2, radius));
     }
 
-    public Block[] getMovedRegion(Vector2f[] coords, int y1, int y2) throws Exception {
+    public Block[] getMovedRegion(Vector3f[] coords) throws Exception {
         List<Block> region = new ArrayList<>();
 
-        for (Vector2f coord : coords) {
-            for (int k = y1; k < y2; k++) {
-                Block block = getBlock((int) coord.x(), k, (int) coord.y());
-                if (block != null) {
+        for (Vector3f coord : coords) {
+            try {
+                Block block = getBlock((int) coord.x(), (int) coord.y(), (int) coord.z());
+                if (block != null && getBlockNeighborCount(block) < 6) {
                     region.add(block);
                 }
+            } catch(Exception e) {
             }
         }
 
         return region.toArray(new Block[0]);
     }
 
-    public Block[] getUpdatedInRange(int x, int z, int radius) {
+    /**
+     * Removes blocks which would be visually hidden as they have neighboring blocks on all 6 sides
+     * @param blocks
+     * @return
+     * @throws Exception
+     */
+    public Block[] removeHiddenBlocks(Block[] blocks) throws Exception
+    {
+        List<Block> region = new ArrayList<>();
+        for(Block block : blocks)
+        {
+            if (block != null && getBlockNeighborCount(block) < 6) {
+                region.add(block);
+            }
+        }
+        return region.toArray(new Block[0]);
+    }
+
+    public Block[] getUpdatedInRange(int x, int y, int z, int radius) {
         if (added.size() < 1)
             return new Block[0];
 
@@ -547,7 +787,8 @@ public class World implements IArea, IPersist {
         for (Block block : added) {
             int gix = (int) block.getPosition().x;
             int giz = (int) block.getPosition().z;
-            if (gix >= (x - radius) && gix <= (x + radius) && giz >= (z - radius) && giz <= (z + radius)) {
+            int giy = (int) block.getPosition().y;
+            if (gix >= (x - radius) && gix <= (x + radius) && giz >= (z - radius) && giz <= (z + radius) && giy >= (y - radius) && giy <= (y + radius)) {
                 region.add(block);
             }
         }
